967c07e83ff14254273bbbe43ea7796ee48397dd
RobotGandhi <99viktor.vc@gmail.com>
2023-03-24 15:18:56 +0100

    Changed remote settings

--- experiments/experiment-hffpi.py
+++ experiments/experiment-hffpi.py
@@ -19,10 +19,10 @@ DOWNWARD_REPO = REPO / "planners" / "downward"
 BENCHMARKS_DIR = REPO / "benchmarks"
 GP_DIR = REPO / "policies"
 if project.REMOTE:
-    SUITE = ["barman", "blocksworld", "childsnack", "clear", "delivery", "depots", "driverlog", "ferry", "freecell", "gripper", "miconic-with-fix", "nomystery", "n-puzzle", "on", "parking", "pipesworld-notankage", "pipesworld-tankage", "reward", "satellite", "sokoban", "spanner", "visitall", "zenotravel"]
+    SUITE = ["transport-thesis"]
     # SUITE = ["barman:p01.pddl", "blocksworld:p01.pddl", "childsnack:p01.pddl", "clear:p-50-0.pddl", "delivery:instance_3_2_0.pddl", "depots:p01.pddl", "driverlog:p01.pddl", "ferry:ferry-l3-c15.pddl", "freecell:p01.pddl", "gripper:p01.pddl", "miconic-with-fix:p01.pddl", "nomystery:p01.pddl", "n-puzzle:n-puzzle-3x3-s1140.pddl", "on:p-50-0.pddl", "parking:p01.pddl", "pipesworld-notankage:p01.pddl", "pipesworld-tankage:p01.pddl", "reward:instance_5x5_0.pddl", "satellite:p01.pddl", "sokoban:p01.pddl", "spanner:pfile01-001.pddl", "visitall:p01.pddl", "zenotravel:p01.pddl"]
     ENV = project.TetralithEnvironment(
-        email="dominik.drexler@liu.se",
+        email="vikca761@student.liu.se",
         extra_options="#SBATCH --account=snic2021-5-330",
         memory_per_cpu="5G")
 else:
